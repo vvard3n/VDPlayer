3b2f76da8afb3bb6d3ffdc86e113b93faa5a9d5c
vvard3n <vvard3n@gmail.com>
2019-04-02 21:32:46 +0800

    first add

---
+++ VDPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VDPlayer
+//
+//  Created by Harwyn T'an on 2019/4/2.
+//  Copyright © 2019年 vvard3n. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VDPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VDPlayer
+//
+//  Created by Harwyn T'an on 2019/4/2.
+//  Copyright © 2019年 vvard3n. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
