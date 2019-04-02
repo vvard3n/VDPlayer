428524033dd2697bc62ae4777fb319569201052c
vvard3n <vvard3n@gmail.com>
2019-04-02 21:41:53 +0800

    swift version

---
+++ Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VDPlayer
+//
+//  Created by Harwyn T'an on 2019/4/2.
+//  Copyright © 2019年 vvard3n. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
