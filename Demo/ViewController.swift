137e33f4bbaf11ac2fcf0958cd7c38b0de3026bb
vvard3n <vvard3n@gmail.com>
2019-04-09 01:18:33 +0800

    🚧

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -33,22 +33,100 @@ class ViewController: UIViewController {
         player?.assetURLs = assetURLs
         player?.fullScreenStateWillChange = { [weak self](player, isFullScreen) in
             self?.setNeedsStatusBarAppearanceUpdate()
+            if #available(iOS 11.0, *) {
+                self?.setNeedsUpdateOfHomeIndicatorAutoHidden()
+            }
+            if isFullScreen {
+//                VDObjcHandle().interfaceOrientation(.landscapeRight)
+                self?.hengp()
+            }
+            else {
+//                VDObjcHandle().interfaceOrientation(.portrait)
+                self?.shup()
+            }
         }
     }
     
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        if player?.isFullScreen ?? false {
-            return .lightContent
+    // MARK: - 横屏
+    let appDelegate = UIApplication.shared.delegate!
+    func hengp() {
+//        appDelegate.blockRotation = true
+        
+        let value = UIInterfaceOrientation.landscapeLeft.rawValue
+        
+        UIDevice.current.setValue(value, forKey: "orientation")
+        
+    }
+    
+    // MARK: - 竖屏
+    
+    func shup() {
+        
+//        appDelegate.blockRotation = false
+        
+        let value = UIInterfaceOrientation.portrait.rawValue
+        
+        UIDevice.current.setValue(value, forKey: "orientation")
+        
+    }
+    
+    
+    
+    // 将要发生旋转就触发代理
+    
+    override func willRotate(to toInterfaceOrientation: UIInterfaceOrientation, duration: TimeInterval) {
+        
+    }
+    
+    // 旋转完成触发代理。我们需要在这里对必要的界面设置重新布局
+    
+    override func didRotate(from fromInterfaceOrientation: UIInterfaceOrientation) {
+        
+        // 获取当前手机物理状态的屏幕模式，看看是横屏还是竖屏.
+        
+        let interfaceOrientation = UIApplication.shared.statusBarOrientation
+        
+        if(interfaceOrientation == UIInterfaceOrientation.portrait) {
+            //当前是在竖屏模式
+            print("竖屏")
         }
+        else{
+            //当前是在横屏模式
+//            self.theWebView?.frame = self.view.frame
+        }
+        
+    }
+    
+    /// 状态栏颜色
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+//        if player?.isFullScreen ?? false {
+//            return .lightContent
+//        }
         return .default
     }
+    
+    /// 自动隐藏Home指示器
+    override var prefersHomeIndicatorAutoHidden: Bool {
+        return player?.isFullScreen ?? false
+    }
 
+    /// 隐藏状态栏
     override var prefersStatusBarHidden: Bool {
         return player?.isFullScreen ?? false
     }
     
-    override var preferredStatusBarUpdateAnimation: UIStatusBarAnimation {
-        return .slide
+    /// 状态栏隐藏动画
+//    override var preferredStatusBarUpdateAnimation: UIStatusBarAnimation {
+//        return .fade
+//    }
+    
+    /// 自动旋转屏幕
+    override var shouldAutorotate: Bool {
+        return false
     }
+    
+//    override var preferredInterfaceOrientationForPresentation: UIInterfaceOrientation {
+//        return .landscapeRight
+//    }
 }
 
---
+++ VDPlayer/Core/VDObjcHandle.h
@@ -0,0 +1,19 @@
+//
+//  VDObjcHandle.h
+//  VDPlayer
+//
+//  Created by Harwyn T'an on 2019/4/9.
+//  Copyright © 2019年 vvard3n. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface VDObjcHandle : NSObject
+
+- (void)interfaceOrientation:(UIInterfaceOrientation)orientation;
+
+@end
+
+NS_ASSUME_NONNULL_END
